cafaaaf78785bc5834301906c3f460f184cb9c6e
rjelolejano7-netizen <jelolejano7@gmail.com>
2026-09-11 18:16:40 +0800

    Add StudentProfile Visual Studio project

---
+++ StudentProfile/Form1.cs
@@ -0,0 +1,46 @@
+namespace StudentProfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Connect the button to the Click event
+            btnEnter.Click += btnEnter_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string studentProfile = txtStudentProfile.Text;
+
+            if (string.IsNullOrWhiteSpace(studentProfile))
+            {
+                MessageBox.Show(
+                    "Please enter your student profile.",
+                    "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                return;
+            }
+
+            MessageBox.Show(
+                "Student Profile - \n\n" + studentProfile,
+                "Result",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void btnEnter_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact Number: 09081154704");
+        }
+    }
+}
